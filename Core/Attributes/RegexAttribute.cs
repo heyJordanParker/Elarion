906f56940ebdfcb499b4b508b4cc99265928737b
Jordan Georgiev <ygeorgiev@objectsystems.com>
2017-06-26 19:21:12 +0300

    Regex Attribute Drawer

---
+++ Core/Attributes/RegexAttribute.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class RegexAttribute : PropertyAttribute {
+    public readonly string helpMessage;
+
+    public readonly string pattern;
+
+    public RegexAttribute(string pattern, string helpMessage) {
+        this.pattern = pattern;
+        this.helpMessage = helpMessage;
+    }
+}
